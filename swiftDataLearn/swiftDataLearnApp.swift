ec0c7eae1fe27fc6834b09719cc023d03f943348
MQamarInam <qamarinam028@gmail.com>
2025-04-23 09:09:53 +0500

    Initial Commit

---
+++ swiftDataLearn/swiftDataLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftDataLearnApp.swift
+//  swiftDataLearn
+//
+//  Created by Qaim's Macbook  on 23/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftDataLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftDataLearnTests/swiftDataLearnTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftDataLearnTests.swift
+//  swiftDataLearnTests
+//
+//  Created by Qaim's Macbook  on 23/04/2025.
+//
+
+import Testing
+@testable import swiftDataLearn
+
+struct swiftDataLearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
