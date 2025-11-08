50a40aaf84674298ed4041443071fff16fe9e77c
MQamarInam <qamarinam028@gmail.com>
2025-11-08 15:54:43 +0500

    added UserDeaults file

---
+++ swiftDataLearn/UserDefaults/UserDeaultsLearn.swift
@@ -0,0 +1,20 @@
+//
+//  Untitled.swift
+//  swiftDataLearn
+//
+//  Created by Qaim's Macbook  on 08/11/2025.
+//
+
+import SwiftUI
+
+struct UserDeaultsLearn: View {
+    var body: some View {
+        
+        Text("qamar")
+        
+    }
+}
+
+#Preview {
+    PersonListView()
+}
